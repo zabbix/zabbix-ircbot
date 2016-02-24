e22ca3d2e28f58c91578e6c5a8ae5140508012c4
richlv <richlv@nakts.net>
2016-02-24 02:57:15 +0200

    ignore addquote command, add networkdiscovery topic

--- ircbot.pl
+++ ircbot.pl
@@ -111,7 +111,7 @@ my %COMMANDS =
     reload=> { function => \&cmd_reload, usage => 'reload - reload topics and keywords'                     },
 );
 
-my @ignored_commands = qw(getquote note quote time seen botsnack);
+my @ignored_commands = qw(getquote note quote time seen botsnack addquote);
 
 sub get_command
 {
